e4bc6d3f7d95297db56f5555cd004c41333360ac
Nate Spilman <nate.spilman@gmail.com>
2023-04-29 09:16:52 -0700

    replacing assets and dding text

--- src/PickAStory/index.tsx
+++ src/PickAStory/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Image, Box } from "@chakra-ui/react";
+import { Button, Image, Box, Text } from "@chakra-ui/react";
 import { ScreenWrapper } from "components/ScreenWrapper/ScreenWrapper";
 import { useRouter } from "next/router";
 
@@ -8,24 +8,106 @@ export const PickAStory = () => {
     <ScreenWrapper screenBg={""}>
       <>
         <Image src="/pick-a-story-bg.png" />
+        <Box
+          position="absolute"
+          top="230px"
+          fontFamily="roboto"
+          lineHeight="1"
+          letterSpacing="tight"
+        >
+          <Text px="4" fontSize="3xs">
+            1.The Player acknowledges and agrees that participating in the game
+            may involve risks of bodily injury, property damage, and other
+            damages or losses. The Player agrees to waive all claims and
+            liabilities arising from or in connection with the Player’s
+            participation in the game, including but not limited to personal
+            injury, death, and property damage. 2. Release of Liability: The
+            Player releases the Owner from all claims, demands, actions, and
+            causes of action arising from or in connection with the Player’s
+            participation in the game. This includes any claims arising from the
+            negligence of the Owner or its agents. 3. Assumption of Risks: The
+            Player acknowledges and agrees that participating in the game
+            involves inherent risks and dangers, including but not limited to
+            falling, slipping, collisions with other players, and other risks
+            associated with physical activity. The Player assumes all risks
+            associated with participation in the game.
+          </Text>
+
+          <Text fontSize="2xs" px="2" py="2">
+            1.By participating in this game, you acknowledge that it involves
+            life-threatening activities that could result in injury, permanent
+            disability, or even death. You are fully responsible for your own
+            safety and well-being, and you assume all risks associated with the
+            game. The creator of this game assumes no liability for any harm
+            that may come to you as a result of your participation.
+          </Text>
+          <Text fontSize="2xs" px="2" py="2">
+            2.You must be at least 18 years of age to participate in this game.
+            Minors are strictly prohibited from taking part in any
+            life-threatening activities, and anyone found to be violating this
+            rule will be immediately disqualified and reported to the
+            authorities.
+          </Text>
+          <Text fontSize="2xs" px="2" py="2">
+            3.You agree to follow all rules and instructions provided by the
+            game creator and/or any authorized personnel. Failure to comply with
+            these rules may result in immediate disqualification and/or removal
+            from the game premises. The creator reserves the right to modify or
+            update the rules at any time, and it is your responsibility to stay
+            informed of any changes.
+          </Text>
+          <Text fontSize="2xs" px="2" py="2">
+            4.By participating in this game, you agree to release the creator
+            from any and all liability, including but not limited to injury,
+            death, property damage, or any other harm that may occur during the
+            game. You also agree to indemnify and hold the creator harmless
+            against any claims or lawsuits brought against them as a result of
+            your participation in the game. This agreement applies to all
+            participants, including any family members or legal representatives
+            who may try to bring a claim on your behalf.
+          </Text>
+        </Box>
+
         <Button
           px="0"
           background="unset"
           _hover={{ background: "unset" }}
           onClick={() => router.push("/the-garden")}
           position="absolute"
-          top="635px"
+          top="935px"
           left="60px"
         >
           <Box
             zIndex={4}
             width="100px"
             height="50px"
-            backgroundImage="unset"
+            backgroundImage="/the-garden-button-no-glow.png"
             backgroundPosition={"center"}
             backgroundSize="cover"
             _hover={{
               backgroundImage: "/garden-button-glow.png",
+            }}
+          />
+        </Button>
+
+        <Button
+          px="0"
+          background="unset"
+          _hover={{ background: "unset" }}
+          onClick={() => router.push("/the-garden")}
+          position="absolute"
+          top="935px"
+          left="160px"
+        >
+          <Box
+            zIndex={4}
+            width="100px"
+            height="50px"
+            backgroundImage="/ugly-boy-button-no-glow.png"
+            backgroundPosition={"center"}
+            backgroundSize="cover"
+            _hover={{
+              backgroundImage: "/ugly-boy-button-glow.png",
             }}
           />
         </Button>
--- src/TheGarden/index.tsx
+++ src/TheGarden/index.tsx
@@ -1,4 +1,4 @@
-import { Image, Box, keyframes } from "@chakra-ui/react";
+import { Image, Box, keyframes, Text, Heading } from "@chakra-ui/react";
 import { ScreenWrapper } from "components/ScreenWrapper/ScreenWrapper";
 import { useRouter } from "next/router";
 
@@ -17,6 +17,36 @@ export const TheGarden = () => {
     <ScreenWrapper screenBg={""}>
       <Box position="relative">
         <Image src="/the-garden-page.png" />
+        <Box
+          position="absolute"
+          top="16"
+          fontFamily="roboto"
+          color="white"
+          px="2"
+        >
+          <Heading fontSize="sm" backgroundColor="red.500" marginRight={28}>
+            Look at this beautiful garden
+          </Heading>
+          <Text fontSize="3xs">
+            Imagine stepping into a serene oasis of natural beauty, surrounded
+            by vibrant colors, delightful scents, and the soothing sounds of a
+            tranquil water feature. This is the world of a luxury garden, where
+            every detail has been carefully crafted to transport you to a place
+            of peace and relaxation. From the moment you enter, you are greeted
+            by a symphony of sights and sounds that will leave you feeling
+            refreshed and rejuvenated. You can stroll along winding paths that
+            meander through lush greenery, pausing to admire the delicate
+            flowers and foliage that line your way. Or, you can find a quiet
+            spot to sit and meditate, soaking in the peaceful ambiance and
+            enjoying the soft rustle of leaves in the breeze. Whether you are
+            seeking a moment of solitude, a romantic setting for a date, or a
+            space to entertain guests, the luxury garden is a perfect choice.
+            It's a breathtakingly beautiful environment that will captivate your
+            senses and transport you to a world of relaxation and tranquility.
+            So come and immerse yourself in the luxury garden experience, and
+            discover the beauty and serenity that awaits you.
+          </Text>
+        </Box>
         <Image
           src="deer-glow2.png"
           position="absolute"
