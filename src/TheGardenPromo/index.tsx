b8183f95e58b8de7723766a14f5db3aa9069aeec
Nate Spilman <nate.spilman@gmail.com>
2023-04-30 18:59:09 -0700

    updating wistia eements

--- src/TheGardenPromo/index.tsx
+++ src/TheGardenPromo/index.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
 import { ScreenWrapper } from "components/ScreenWrapper/ScreenWrapper";
 import { useRouter } from "next/router";
 
@@ -7,15 +7,91 @@ export const TheGardenPromo = () => {
   return (
     <ScreenWrapper screenBg={""}>
       <Box width="full" height="full" background="black" display="flex">
-        <iframe
-          width="100%"
-          src={"https://www.youtube.com/embed/F2SftCkhH3Y"}
-          title="The Garden Promo pt 1"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        />
+        <VideoComponent />
       </Box>
     </ScreenWrapper>
   );
 };
+
+import React, { useEffect, useRef } from "react";
+
+declare global {
+  interface Window {
+    _wq: any[];
+  }
+}
+
+const VideoComponent: React.FC = () => {
+  const isLoaded = useRef(true);
+  useEffect(() => {
+    if (isLoaded.current) {
+      isLoaded.current = false;
+      const script = document.createElement("script");
+      script.src = "https://fast.wistia.com/assets/external/E-v1.js";
+      script.async = true;
+      document.body.appendChild(script);
+
+      script.addEventListener("load", () => {
+        window._wq = window._wq || [];
+        window._wq.push({
+          id: "lqd5lvcti5",
+          onReady: function (video: any) {
+            video.bind("end", function () {
+              console.log("Video Ended!");
+              // perform your action here
+            });
+          },
+        });
+      });
+
+      return () => {
+        document.body.removeChild(script);
+      };
+    }
+  }, []);
+
+  const parent = useRef<HTMLDivElement>(null);
+
+  return (
+    <Box
+      className="wistia_embed wistia_async_lqd5lvcti5"
+      position="relative"
+      height="100vh"
+      width="100vw"
+    >
+      <Box
+        className="wistia_swatch"
+        style={{
+          height: "100%",
+          left: "0",
+          opacity: "0",
+          overflow: "hidden",
+          position: "absolute",
+          top: "0",
+          transition: "opacity 200ms",
+          width: "100%",
+        }}
+        ref={parent}
+      >
+        <Image
+          src={`https://fast.wistia.com/embed/medias/lqd5lvcti5/swatch`}
+          style={{
+            filter: "blur(5px)",
+            height: "100%",
+            objectFit: "contain",
+            width: "100%",
+          }}
+          alt=""
+          aria-hidden="true"
+          onLoad={() => {
+            if (parent.current) {
+              parent.current.style.opacity = "1";
+            }
+          }}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default VideoComponent;
