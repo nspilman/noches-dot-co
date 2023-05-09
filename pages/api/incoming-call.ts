81e57964a25658e76381d50477663b3892b895a7
Nate Spilman <nate.spilman@gmail.com>
2023-05-08 21:03:36 -0700

    abstracting number adding logic to util function

--- pages/api/incoming-call.ts
+++ pages/api/incoming-call.ts
@@ -1,19 +1,12 @@
 // pages/api/incoming_call.ts
 
 import type { NextApiRequest, NextApiResponse } from "next";
-import twilio from "twilio";
 import VoiceResponse from "twilio/lib/twiml/VoiceResponse";
-import { getSupabaseClient } from "utils/getSupabaseClient";
+import { handleIncomingNumber } from "utils/handleIcomingNumber";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     try {
-      const client = twilio(
-        process.env.NEXT_PUBLIC_TWILIO_ACCOUNT_SID,
-        process.env.NEXT_PUBLIC_TWILIO_AUTH_TOKEN
-      );
-      const supabase = getSupabaseClient();
-
       const twiml = new VoiceResponse();
       twiml.play("/noches-voicemail.mp3");
       //   twiml.say({ voice: "alice" }, "we out here");
@@ -21,27 +14,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       const messageText =
         "Having trouble waking up? https://noches.co can help. You are not in danger";
 
-      const to = req.body.From;
-      console.log({ to });
-      const { data: previousTableEntriesForNumber } = await supabase
-        .from("marketing_incoming_calls")
-        .select("*")
-        .filter("user_number", "eq", to);
-
-      if (!previousTableEntriesForNumber?.length) {
-        await client.messages.create({
-          body: messageText,
-          from: process.env.NEXT_PUBLIC_TWILIO_NUMBER,
-          to,
-        });
-      }
-
-      const { status } = await supabase
-        .from("marketing_incoming_calls")
-        .insert({
-          user_number: to,
-          noches_number: process.env.NEXT_PUBLIC_TWILIO_NUMBER,
-        });
+      handleIncomingNumber(req.body.From, messageText);
 
       res.setHeader("Content-Type", "text/xml");
       res.status(200).send(twiml.toString());
---
+++ utils/handleIcomingNumber.ts
@@ -0,0 +1,30 @@
+import twilio from "twilio";
+import { getSupabaseClient } from "./getSupabaseClient";
+
+export const handleIncomingNumber = async (to: string, messageText: string) => {
+  const supabase = getSupabaseClient();
+  const { data: previousTableEntriesForNumber, error } = await supabase
+    .from("marketing_incoming_calls")
+    .select("*")
+    .filter("user_number", "eq", to);
+
+  const client = twilio(
+    process.env.NEXT_PUBLIC_TWILIO_ACCOUNT_SID,
+    process.env.NEXT_PUBLIC_TWILIO_AUTH_TOKEN
+  );
+
+  if (!previousTableEntriesForNumber?.length) {
+    await client.messages.create({
+      body: messageText,
+      from: process.env.NEXT_PUBLIC_TWILIO_NUMBER,
+      to,
+    });
+  }
+
+  const { status } = await supabase
+    .from("marketing_incoming_calls")
+    .insert({
+      user_number: to,
+      noches_number: process.env.NEXT_PUBLIC_TWILIO_NUMBER,
+    });
+};
